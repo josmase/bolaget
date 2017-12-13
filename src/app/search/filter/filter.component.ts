3483892148ba7838f970e171bf01b2d06ce5ee59
josmase <josnase@gmail.com>
2017-12-13 22:18:04 +0100

    Fix selection having zero options

--- src/app/search/filter/filter.component.ts
+++ src/app/search/filter/filter.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {ProductsService} from '../products.service';
 
+
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
@@ -9,6 +10,7 @@ import {ProductsService} from '../products.service';
 })
 export class FilterComponent implements OnInit {
   filterForm: FormGroup;
+  categories: any[];
 
   constructor(private formBuilder: FormBuilder, private products: ProductsService) {
     this.createForm();
@@ -17,6 +19,21 @@ export class FilterComponent implements OnInit {
   ngOnInit() {
     this.onChanges();
     this.products.requestProducts(this.filterForm.getRawValue());
+    this.categories = [{value: 'Sprit', name: 'Sprit'},
+      {value: 'Öl', name: 'Öl'},
+      {value: 'Whisky', name: 'Whisky'},
+      {value: 'Cider', name: 'Cider'},
+      {value: 'Rött vin', name: 'Rött vin'},
+      {value: 'Vitt vin', name: 'Vitt vin'},
+      {value: 'Tequila', name: 'Tequila'},
+      {value: 'Mousserande vin', name: 'Mousserande vin'},
+      {value: 'Likör', name: 'Likör'},
+      {value: 'Cognac', name: 'Cognac'},
+      {value: 'Sherry', name: 'Sherry'},
+      {value: 'Portvin', name: 'Portvin'},
+      {value: 'Gin', name: 'Gin'},
+      {value: 'Brandy', name: 'Brandy'},
+      {value: 'Armagnac', name: 'Armagnac'}];
   }
 
   /**
@@ -33,20 +50,21 @@ export class FilterComponent implements OnInit {
    */
   private createForm() {
     this.filterForm = this.formBuilder.group({
-      price: this.formBuilder.group({
-        min: null as number,
-        max: null as number
-      }),
-      apk: this.formBuilder.group({
-        min: null as number,
-        max: null as number
-      }),
-      alcohol: this.formBuilder.group({
-        min: null as number,
-        max: null as number
-      }),
-      name: null as string,
-      category: null as string
-    });
+        price: this.formBuilder.group({
+          min: null as number,
+          max: null as number
+        }),
+        apk: this.formBuilder.group({
+          min: null as number,
+          max: null as number
+        }),
+        alcohol: this.formBuilder.group({
+          min: null as number,
+          max: null as number
+        }),
+        name: null as string,
+        category: null as string
+      }
+    );
   }
 }
