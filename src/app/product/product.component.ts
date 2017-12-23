0d085265d00e7be0569dcbd287069a7f0d237f2d
josmase <josnase@gmail.com>
2017-12-23 12:10:17 +0100

    Setup retrieval of product info

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {ProductsService} from '../search/products.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -6,11 +8,30 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  private sub: any;
+  private product;
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private productsService: ProductsService) {
   }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const id = parseInt(params['id'], 10);
+      this.getProduct(id);
+    });
+  }
+
+  /**
+   * Gets the product either from aöready retrived data or by making a new request for data.
+   * @param {Number} id The id to find.
+   */
+  private getProduct(id: Number) {
+    this.product = this.productsService.getProduct(id);
+    if (this.product === undefined || this.product === null) {
+      this.productsService.requestProduct(id).then(data => {
+        this.product = data;
+      });
+    }
   }
 
 }
