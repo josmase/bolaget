041c764c94006758704e99a7b1f1a36a5abd094f
josmase <josnase@gmail.com>
2018-01-01 15:39:45 +0100

    use hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import {SearchComponent} from './search/search.component';
 import {ProductComponent} from './product/product.component';
 
 const routes: Routes = [
-  {path: 'search', component: SearchComponent},
+  {path: '', component: SearchComponent},
   {path: 'product/:id', component: ProductComponent},
-  {path: '', redirectTo: 'search', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
