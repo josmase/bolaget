94677ef31fd1444ec81dc9b4cfe614e7e7a170e9
josmase <josnase@gmail.com>
2018-01-01 16:22:11 +0100

    Use search of product number instead of trying to get the actual product url.

--- src/app/product/to-url.pipe.ts
+++ src/app/product/to-url.pipe.ts
@@ -7,29 +7,7 @@ import {Product} from '../search/product';
 export class ToUrlPipe implements PipeTransform {
 
   transform(value: Product, args?: any): String {
-    const base = 'https://www.systembolaget.se/dryck/';
-    const category = this.formatString(value.Varugrupp);
-    const name = this.formatString(value.Namn);
-
-    return base + category + '/' + name + '-' + value.nr;
+    const base = 'https://www.systembolaget.se/sok-dryck/?searchquery=';
+    return base + value.nr + '&fullassortment=1';
   }
-
-  /**
-   * Transform the string to lowercase with spaces replaced with '-' and å,ä,ö replaced with a,a,o.
-   * @param {String} s The String to format.
-   * @returns {string} Formatted string.
-   */
-  private formatString(s: String) {
-    return this.replaceSwedishChars(s.toLowerCase().replace(/ /g, '-'));
-  }
-
-  /**
-   * Replaces å,ä,ö with a,a,o. Input most be lowercase.
-   * @param {String} s The string to replace in.
-   * @returns {string} String with å,ä,ö replaced.
-   */
-  private replaceSwedishChars(s: String) {
-    return s.replace(/å/g, 'a').replace(/ä/g, 'a').replace(/ö/g, 'o');
-  }
-
 }
