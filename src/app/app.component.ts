23ffa59e9e1832c14a9dab60433bcae1156ec038
josmase <josnase@gmail.com>
2018-08-16 21:07:17 +0200

    Add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {transition, trigger, useAnimation} from '@angular/animations';
 import {RouteAnimations} from './animations/route-animations';
+import {NavigationEnd, Router} from '@angular/router';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,17 @@ import {RouteAnimations} from './animations/route-animations';
   ]
 })
 export class AppComponent {
-  getRouteAnimation(outlet) {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && environment.production) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+        // console.log('Sending view');
+      }
+    });
+  }
+
+  static getRouteAnimation(outlet) {
     return outlet.activatedRouteData.animation;
   }
 }
